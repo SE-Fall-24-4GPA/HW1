04c662732952d6680de57ff304295dfe038eded4
swapnil <swapnil99jakhi@gmail.com>
2024-08-28 00:51:38 -0400

    last commit

--- tests/test_sample2.py
+++ tests/test_sample2.py
@@ -3,4 +3,4 @@ import os
 sys.path.append((os.path.join(os.path.dirname(__file__), '..')))
 from myfile import factorial
 def test_case():
-    assert factorial(4) == 24
+    assert factorial(4) == 25
